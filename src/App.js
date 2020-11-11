564407813928dd4c2dd981ca05db0356a747581d
Babin Brahmachari <kingsuk.brahmachari@gmail.com>
2020-11-11 11:07:31 +0530

    Added constants file for redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header/header.component';
@@ -78,7 +78,10 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          {/* <Route path='/signin' component={SignInAndSignUpPage} /> */}
+          <Route path='/signin' render={()=>this.props.currentUser?
+          (<Redirect to ="/"/>):(<SignInAndSignUpPage/>) }/>
+         
           
         </Switch>
       </div>
@@ -87,11 +90,13 @@ class App extends React.Component{
   }
  
 }
-
+const mapStateToProps =(state)=>({
+  currentUser:state.user.currentUser
+})
 const mapDispatchToProps=(dispatch)=>{
   return{
     setCurrentUser:user =>dispatch(setCurrentUser(user))
   }
 }
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
