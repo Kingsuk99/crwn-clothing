1162086bf53c4063d5ae1beb5aa5dc295017b8f6
Babin Brahmachari <kingsuk.brahmachari@gmail.com>
2021-03-13 13:13:44 +0530

    Seperated folder for server and client

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
 
 const StripeCheckoutButton=({price})=>{
     const priceForStripe=price *100;
-    const onToken =token =>{
-        console.log('token',token);
-        alert('Payment Successful');
-    }
     const publishableKey ='pk_test_51HyW42BuYyZGs9dM1JOGD1qmE8rln7TNmP7x1XXKQFUMPYQ9yXOVRfcNG9njz60jzJUj8qrGhIl5zgiIiGt40MDK00AD9iYATm';
+    const onToken = token => {
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
+ 
     return(
         <div>
        
